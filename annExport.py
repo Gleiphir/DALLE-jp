b1eef5c83f76c48dd731402db426f2b3e0217ca0
Gleiphir <zhangzx.1997@163.com>
2021-02-13 05:00:29 +0900

    Update annExport.py

--- annExport.py
+++ annExport.py
@@ -1,17 +1,17 @@
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 cap = dset.CocoCaptions(root = './coco/images',
-                        annFile = './coco/annotations/image_info_test2017.json',
-                        transform=transforms.ToTensor())
+                        annFile = './coco/annotations/image_info_test2017.json',)
+                        #transform=transforms.ToTensor())
 
 print('Number of samples: ', len(cap))
 #img, target = cap[3] # load 4th sample
 L = []
 
 for i, (img, target) in enumerate(cap):
+    print("# {}".format(i))
     for s in target:
         L.append("{}|{}".format(i,s))
-        print("# {}".format(i))
 
 with open("ann_export.txt","w",encoding='utf-8') as Fp:
     Fp.writelines(L)
