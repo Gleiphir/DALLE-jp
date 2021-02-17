5b9d497d5a1c9206be4b21339fedcd3abd0b4107
Gleiphir <zhangzx.1997@163.com>
2021-02-17 13:31:07 +0900

    Update DsetDemo.py

--- DsetDemo.py
+++ DsetDemo.py
@@ -25,7 +25,7 @@ print('Max len',tokenDset.maxLen())
 
 
 for i, (img, target) in enumerate(cap):
-    print(i,":",tokenDset.getRand(i),img.size)
+    print(i,":",tokenDset.getRand(i),img.size())
     if i > 10:
         break
 
