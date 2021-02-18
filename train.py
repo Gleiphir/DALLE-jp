79d7614e5c14495b22e193b9ce9d27564f8d38dd
Gleiphir <zhangzx.1997@163.com>
2021-02-18 16:50:07 +0900

    Update train.py

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ for i, (img, target) in enumerate(loader):
     loss = vae(img.cuda(),return_recon_loss = True)
     loss.backward()
     if i > 100: break
-    
+
 torch.save(vae.state_dict(),"Vae.pth")
 
 dalle = DALLE(
@@ -87,7 +87,7 @@ loader = DataLoader(cap)
 for i, (img, target) in enumerate(loader):
     print("DALLE epoch {} / {}".format(i, len(loader)))
 
-    textToken, mask = fixlen( [ tokenDset.tokenizeList(token_dataset.getRand(i)) ])
+    textToken, mask = fixlen( [ tokenDset.tokenizeList( tokenDset.getRand(i)  ) ])
     loss = dalle(textToken.cuda(), img.cuda(), mask = mask.cuda(), return_loss = True)
     loss.backward()
     if i >100:break
