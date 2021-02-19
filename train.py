230cbd2a7b5b307f3b77ef31d835d19e0e8c924f
Gleiphir <zhangzx.1997@163.com>
2021-02-19 16:42:50 +0900

    Update train.py

--- train.py
+++ train.py
@@ -53,6 +53,7 @@ cap = dset.CocoCaptions(root = './coco/images',
                         transform=transforms.Compose([
                             transforms.Resize((IMAGE_SIZE,IMAGE_SIZE)),
                             transforms.ToTensor(),
+                            transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
                         ]))
 
 loader = DataLoader(cap)
