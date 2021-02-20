e1ed120d62716164deacc699a1158f3ddea5fb7e
Gleiphir <zhangzx.1997@163.com>
2021-02-20 20:58:17 +0900

    Update train_small.py

--- train_small.py
+++ train_small.py
@@ -22,7 +22,7 @@ TRAIN_BATCHES = 100
 #Edit: And yup, you need to reserve 0 for padding and 1 for , so add 2 to your encoded text ids!
 
 
-EPOCHS = 3
+EPOCHS = 100
 
 
 
@@ -68,7 +68,7 @@ for epoch in range(EPOCHS):
         img=img.unsqueeze(0).cuda()
         #print(img.size())
         if i %10 == 0:
-            print("VAE epoch {} / {}".format(i,EPOCHS * 100))
+            print("VAE epoch {} / {}".format(i+ epoch*100,EPOCHS * 100))
         loss = vae(img,return_recon_loss = True)
         loss.backward()
 
@@ -95,7 +95,7 @@ for epoch in range(EPOCHS):
         #print(img.size())
         img = img.unsqueeze(0).cuda()
         if i % 10 == 0:
-            print("DALLE epoch {} / {}".format(i, EPOCHS * 100))
+            print("DALLE epoch {} / {}".format(i+epoch*100, EPOCHS * 100))
         try:
             textToken, mask = fixlen([tokenDset.getRand(i)])
         except KeyError:
