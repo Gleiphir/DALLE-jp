9aac7992b6824e98f778e6d9655db5d67aec0a6c
Gleiphir <zhangzx.1997@163.com>
2021-02-21 14:33:54 +0900

    Update train_small.py

--- train_small.py
+++ train_small.py
@@ -6,7 +6,7 @@ from dalle_pytorch import DiscreteVAE, DALLE
 import numpy as np
 from torch.utils.data.dataloader import DataLoader
 import random
-
+from PIL import Image
 
 IMAGE_SIZE = 256 # 256*256
 
@@ -51,7 +51,7 @@ mask = torch.ones_like(text).bool()
 cap = dset.CocoCaptions(root = './coco/images',
                         annFile = './coco/annotations/captions_val2014.json',
                         transform=transforms.Compose([
-                            transforms.Resize((IMAGE_SIZE,IMAGE_SIZE)),
+                            transforms.Resize((IMAGE_SIZE,IMAGE_SIZE),Image.NEAREST),
                             transforms.ToTensor(),
                             transforms.Normalize( (0.5,0.5,0.5), (0.5,0.5,0.5))
                         ]))
