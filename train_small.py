900c485226d9884a03bfa4163d52916549fc38c0
Gleiphir <zhangzx.1997@163.com>
2021-03-01 18:24:35 +0900

    Update train_small.py

--- train_small.py
+++ train_small.py
@@ -55,7 +55,8 @@ mask = torch.ones_like(text).bool()
 cap = dset.CocoCaptions(root = './coco/images',
                         annFile = './coco/annotations/captions_val2014.json',
                         transform=transforms.Compose([
-                            transforms.Resize((IMAGE_SIZE,IMAGE_SIZE),Image.NEAREST),
+                            transforms.RandomCrop((IMAGE_SIZE,IMAGE_SIZE)),
+                            #transforms.Resize((IMAGE_SIZE,IMAGE_SIZE),Image.NEAREST),
                             transforms.ToTensor(),
                             transforms.Normalize( (0.5,0.5,0.5), (0.5,0.5,0.5))
                         ]))
